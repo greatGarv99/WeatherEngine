ff6c08f74c7c5a578ed6f4a0cc940d2220f5f428
TheGreatGarv <garv.programming@gmail.com>
2020-05-11 13:56:05 +0530

    Registered a model, made a basic home-page and a basic home url that renders home page on request

--- weatherScraper/models.py
+++ weatherScraper/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class WeatherQuery(models.Model):
+    location = models.CharField(max_length=200)
+    def __str__(self):
+        return self.location
+
+    class Meta:
+        verbose_name_plural = "Weather Queries"
